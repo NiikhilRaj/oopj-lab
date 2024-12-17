719cf66199eafda8b7bfa6a7f8710e5182fc4d0f
Nikhil Raj <nikhilraj13733@gmail.com>
2024-12-17 17:54:10 +0530

    day2

--- Day2/lab2C.java
+++ Day2/lab2C.java
@@ -1,3 +1,22 @@
+import java.io.IOException;
+import java.util.*;
+
 public class lab2C {
-    
+    public static void main(String args[]) throws IOException {
+        if (args.length - 1 != Integer.parseInt(args[0])) {
+            System.out.println("Enter " + args[0] + " numbers only.");
+            return;
+        }
+        int[] arr = new int[Integer.parseInt(args[0])];
+        for (int i = 1; i <= Integer.parseInt(args[0]); i++) {
+            try {
+                arr[i - 1] = Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a valid number");
+                return;
+            }
+        }
+        Arrays.sort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
 }
--- Day2/lab2D.java
+++ Day2/lab2D.java
@@ -1,3 +1,22 @@
+import java.util.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class lab2D {
-    
+    public static void main(String[] args)throws IOException {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("ENter the suze of the array:");
+        String s = scanner.readLine();
+
+        int size = 0;
+        try {
+            size = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            System.out.println("Enter a valid integer for size of the array.");
+            return;
+        }
+        int[] arr = new int[size];
+        Arrays.sort(arr);
+        //code not complete
+    }
 }
